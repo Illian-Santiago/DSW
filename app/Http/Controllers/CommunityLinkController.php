54a2c4310a5a64021d2751b664898d161436e204
Illian-Santiago <illianclase12345678@gmail.com>
2024-11-11 10:38:59 +0000

    Todo funciona, por separado

--- app/Http/Controllers/CommunityLinkController.php
+++ app/Http/Controllers/CommunityLinkController.php
@@ -16,34 +16,36 @@ class CommunityLinkController extends Controller
      */
     public function index(Channel $channel = null)
     {
-        if (request()->exists('popular')) {
-            if ($channel) {
-                // Obtener los enlaces más populares dentro de un mismo canal
-                $links = (new CommunityLinkQuery())->getMostPopularByChannel($channel);
-            } else {
-                // Obtener los enlaces más populares en general
-                $links = (new CommunityLinkQuery())->getMostPopular();
-            }
-        } else {
-            if ($channel) {
-                // Obtener los enlaces más recientes dentro del canal
-                $links = (new CommunityLinkQuery())->getByChannel($channel);
-            } else {
-                // Obtener los enlaces más recientes en general
-                $links = (new CommunityLinkQuery())->getAll();
-            }
-        }
-
         $channels = Channel::orderBy('title', 'asc')->get();
+        $query = new CommunityLinkQuery();
+
+        switch (true) {
+            case request()->exists('inputField'):
+                $links = $query->buscarPorNombre(request()->get('inputField'));
+                break;
+
+            case request()->exists('popular'):
+                $links = $query->getMostPopular();
+                break;
+
+            case $channel:
+                $links = $query->getByChannel($channel);
+                break;
+
+            default:
+                $links = $query->getAll();
+                break;
+        }
 
         return view('dashboard', compact('links', 'channels'));
     }
+
 
     public function myLinks()
     {
         $user = Auth::user();
         $links = $user->myLinks()->paginate(10);
-        
+
         return view('myLinks', compact('links'));
     }
 
@@ -64,18 +66,16 @@ class CommunityLinkController extends Controller
         $link = new CommunityLink($data);
 
         $existing = $link->hasAlreadyBeenSubmitted();
-        
+
         if (!$existing) {
             $link->user_id = Auth::id();
             $link->approved = Auth::user()->trusted ?? false;
-            
+
             $link->save();
-    
+
             if (!Auth::user()->trusted) {
                 return back()->with('info', 'Your link is under review for approval.');
             }
-
-            
         }
         return back();
     }
--- app/Queries/CommunityLinkQuery.php
+++ app/Queries/CommunityLinkQuery.php
@@ -5,43 +5,47 @@ namespace App\Queries;
 use App\Models\Channel;
 use App\Models\CommunityLink;
 
-class CommunityLinkQuery {
-    public function getByChannel(Channel $channel) {
-        //Filtrar los links por el canal
-        $links = $channel->communityLinks()
-        ->where('approved', true)
-        ->latest('updated_at')
-        ->paginate(10);
-
-        return $links;
-    }
-    
-    public function getAll() {
+class CommunityLinkQuery
+{
+    public function getAll()
+    {
         // Mostrar todos los links que esten aprovados
-        $links = CommunityLink::where('approved', true)
-        ->latest('updated_at')
-        ->paginate(10);
+        $links = CommunityLink::where('approved', 1)
+            ->latest('updated_at')
+            ->paginate(10);
 
         return $links;
     }
 
-    public function getMostPopular() {
+    public function getMostPopular()
+    {
         //Muestra los links que han sido votados
         $links = CommunityLink::Where('approved', 1)
-        ->withCount('users')
-        ->orderby('users_count', 'desc')
-        ->paginate(10);
+            ->withCount('users')
+            ->orderby('users_count', 'desc')
+            ->paginate(10);
 
         return $links;
     }
 
-    public function getMostPopularByChannel(Channel $channel)
+    public function buscarPorNombre(string $input)
     {
+        $links = CommunityLink::where('approved', 1) // De los links que estan aprovados
+            ->where('title', 'like', '%' . $input . '%') // Buscamos esque contenga el input del usuario
+            ->latest('updated_at')
+            ->paginate(10);
+
+        return $links;
+    }
+
+    public function getByChannel(Channel $channel)
+    {
+        // Filtrar los links por el canal
         $links = $channel->communityLinks()
             ->where('approved', true)
-            ->withCount('users')
-            ->orderBy('users_count', 'desc')
+            ->latest('updated_at')
             ->paginate(10);
+
         return $links;
     }
 }
