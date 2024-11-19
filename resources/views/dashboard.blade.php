4e97412fff858bbb2e9c711a68acf7d0b240456a
Illian-Santiago <illianclase12345678@gmail.com>
2024-11-19 11:22:35 +0000

    A23. Añadir autenticación a la API

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Community Contributions') }}
-            </h2>
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Community Contributions') }}
+        </h2>
+
+        @if (session('token'))
+        <p>Token: {{ session('token') }}</p>
+        @endif
     </x-slot>
-    <x-alert_user_status/>
+    <x-alert_user_status />
     <div class="mt-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg p-4">
-        <x-community-add-link :channels="$channels"/>
-        <x-community-links :links="$links"/>
+        <x-community-add-link :channels="$channels" />
+        <x-community-links :links="$links" />
     </div>
 </x-app-layout>
