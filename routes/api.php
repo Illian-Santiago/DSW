1619c2a6c709b688f8c80b3a90a20af5b35f18b2
Illian-Santiago <illianclase12345678@gmail.com>
2024-11-19 11:22:35 +0000

    Ejercicio 1. Proteger endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\v1\CommunityLinkController;;
+use App\Http\Controllers\Api\v1\CommunityLinkController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('v1/communitylinks', CommunityLinkController::class);
+Route::apiResource('v1/communitylinks', CommunityLinkController::class)
+    ->middleware(['verified', 'auth:sanctum']);
